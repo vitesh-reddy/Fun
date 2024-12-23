c5a342ca219e4663d9adc0f5539d359d5c099cd3
vitesh-reddy <k.l.viteshreddy@gmail.com>
2024-12-23 23:28:47 +0530

    fix video

--- src/Components/Video.jsx
+++ src/Components/Video.jsx
@@ -4,7 +4,7 @@ import fun from "../assets/fun.mp4"
 const Video = () => {
   return (
     <div className='flex items-center justify-center bg-[rgb(0_8_28)] h-full w-full my-100'>
-      <video src={fun} controls autoPlay={true}></video>
+      <video src={fun} autoPlay={true}></video>
     </div>
   )
 }
